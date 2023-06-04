4e59f1ead70757b76f57058a3a0a208713d25488
thiago_osim <thiago_osim@hotmail.com>
2023-06-04 20:59:20 -0300

    Realizado a implementação do método UpdateAsync da classe BaseRepository

--- src/Api.Data/Repository/BaseRepository.cs
+++ src/Api.Data/Repository/BaseRepository.cs
@@ -16,9 +16,24 @@ namespace Api.Data.Repository
             _dataset = context.Set<T>();
         }
 
-        public Task<bool> DeleteAsync(Guid id)
+        public async Task<bool> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync(p => p.Id.Equals(id));
+                if (result == null)
+                {
+                    return false;
+                }
+
+                _dataset.Remove(result);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public async Task<T> InsertAsync(T item)
