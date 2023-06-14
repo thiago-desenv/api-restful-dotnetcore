4d759b95312d56559a9279ec46f31bf4ddf206a1
thiago_osim <thiago_osim@hotmail.com>
2023-06-14 20:53:45 -0300

    Implementado a interface IUserService na classe UserService

--- src/Api.Service/Services/UserService.cs
+++ src/Api.Service/Services/UserService.cs
@@ -1,7 +1,33 @@
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+
 namespace Api.Service.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<UserEntity> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserEntity> Post(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserEntity> Put(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
