148e09a6509cced3d55ec2820a02c0d2984abb3e
thiago_osim <thiago_osim@hotmail.com>
2023-07-08 22:43:10 -0300

    Adcionado injeção do banco de dados na classe ConfigureRepository

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,5 +1,7 @@
+using Api.Data.Context;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.CrossCutting.DependencyInjection
@@ -9,6 +11,12 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesServicesRepository(IServiceCollection serviceColletion)
         {
             serviceColletion.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceColletion.AddDbContext<MyContext>
+            (
+                options => options.UseMySql(
+                    "Server=LocalHost;Port=3306;DataBase=dbAPI;Uid=root;Pwd=1234",
+                    new MySqlServerVersion(new Version("8.0.33")))
+            );
         }
     }
 }
